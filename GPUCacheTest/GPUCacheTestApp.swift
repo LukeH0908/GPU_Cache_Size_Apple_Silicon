9d65aacc852a625d8fc83073a9217a779fed9299
Luke He <lhe15@u.rochester.edu>
2025-08-19 21:26:49 -0400

    Initial Commit

---
+++ GPUCacheTest/GPUCacheTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GPUCacheTestApp.swift
+//  GPUCacheTest
+//
+//  Created by Luke He on 8/19/25.
+//
+
+import SwiftUI
+
+@main
+struct GPUCacheTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GPUCacheTestTests/GPUCacheTestTests.swift
@@ -0,0 +1,17 @@
+//
+//  GPUCacheTestTests.swift
+//  GPUCacheTestTests
+//
+//  Created by Luke He on 8/19/25.
+//
+
+import Testing
+@testable import GPUCacheTest
+
+struct GPUCacheTestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
